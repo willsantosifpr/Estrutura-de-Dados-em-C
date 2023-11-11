37c5cde3bef0e3f8297bbff8970bb9ead654af6a
William C S Santos <willappleid@yahoo.com>
2023-11-11 16:48:25 -0300

    Estrutura de Dados em C

---
+++ 3. Variáveis e tipos de dados/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//William Cardoso da Silva Santos - Nov/8/2023
+
+int a = 5;
+float num = 2.3;
+double x = 4.6;
+char y = 'W';
+
+int main()
+{
+    printf("\n O valor da variavel 'a' eh: %d\n", a);
+    printf(" O valor da variavel 'num' eh: %.1f\n", num);
+    printf(" O valor da variavel 'x' eh: %.1f\n", x);
+    printf(" O valor da variavel 'y' eh: %c\n", y);
+
+    return 0;
+}
