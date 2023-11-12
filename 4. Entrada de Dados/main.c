e6e08d4f69b456cf49ab93a43643b13b52073c21
William C S Santos <willappleid@yahoo.com>
2023-11-12 16:53:46 -0300

    Estrutura de Dados em C

---
+++ 4. Entrada de Dados/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//William Cardoso da Silva Santos - Nov/12/2023
+
+int main()
+{
+    int num, soma;
+
+    printf("\n Digite um numero: ");
+    scanf("%d", &num);
+
+    printf(" O valor da variavel 'num' eh: %d\n", num);
+
+    soma = num + 5;
+
+    printf(" O valor da variavel 'num' + 5 eh: %d\n", soma);
+
+    return 0;
+}
