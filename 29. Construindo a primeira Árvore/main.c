b97fd182cda3257d53d09d0ec66246119795c79e
William C S Santos <willappleid@yahoo.com>
2023-11-27 16:19:57 -0300

    Estrutura de Dados em C

---
+++ 29. Construindo a primeira Árvore/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//William Cardoso da Silva Santos - Nov/27/2023
+
+struct nodo
+{
+    int valor;
+    struct nodo *esq;
+    struct nodo *dir;
+};
+
+typedef struct nodo Nodo;
+
+Nodo* create(int valor)
+{
+    Nodo *n = malloc(sizeof(Nodo));
+
+    n->valor = valor;
+    n->esq = NULL;
+    n->dir = NULL;
+    return n;
+}
+
+int main()
+{
+    Nodo *nodo5 = create(5);
+    Nodo *nodo2 = create(2);
+    Nodo *nodo1 = create(1);
+    Nodo *nodo8 = create(8);
+    Nodo *nodo4 = create(4);
+
+    nodo5->esq = nodo2;
+    nodo2->esq = nodo1;
+    nodo5->dir = nodo8;
+    nodo2->dir = nodo4;
+
+    return 0;
+}
